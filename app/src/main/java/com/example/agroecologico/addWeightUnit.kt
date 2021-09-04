7ac4c62fb65db881d9b8c78582d6bf06f56df79f
Angelica <aagudelo@truora.com>
2021-09-03 22:39:06 -0500

    add weigth unit view

---
+++ app/src/main/java/com/example/agroecologico/addWeightUnit.kt
@@ -0,0 +1,36 @@
+package com.example.agroecologico
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.agroecologico.databinding.ActivityAddWeightUnitBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class addWeightUnit : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityAddWeightUnitBinding
+    private lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityAddWeightUnitBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.btnAddUnit.setOnClickListener{
+            val unitValue = viewBinding.tfUnit.text.toString()
+
+            database = FirebaseDatabase.getInstance().getReference("Unit")
+
+            database.child(unitValue).setValue(unitValue).addOnSuccessListener {
+                viewBinding.tfUnit.text.clear()
+
+                Toast.makeText(this,"La unidad de venta se almacenó exitosamente", Toast.LENGTH_SHORT).show()
+
+            }.addOnFailureListener{
+
+                Toast.makeText(this,"La unidad de venta no pudo ser almacenada", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+}
