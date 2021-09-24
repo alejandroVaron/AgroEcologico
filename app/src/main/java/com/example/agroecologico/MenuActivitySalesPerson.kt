89da0185b580c760fa3c641e5291fa0e018affd9
Angelica <aagudelo@truora.com>
2021-09-23 22:08:10 -0500

    add fragment to include a sales person

--- app/src/main/java/com/example/agroecologico/MenuActivitySalesPerson.kt
+++ app/src/main/java/com/example/agroecologico/MenuActivitySalesPerson.kt
@@ -1,32 +1,20 @@
 package com.example.agroecologico
 
-import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
-import android.os.AsyncTask
-import android.os.AsyncTask.execute
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
-import android.view.MenuItem
 import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.agroecologico.Fragments.*
 import com.example.agroecologico.Models.MarketStall
 import com.example.agroecologico.Models.Product
-import com.example.agroecologico.databinding.ActivityMenuAdminBinding
 import com.example.agroecologico.databinding.ActivityMenuSalesPersonBinding
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.database.DatabaseReference
@@ -76,7 +64,7 @@ class MenuActivitySalesPerson : AppCompatActivity() {
 
                     true
                 }
-                R.id.itemAddProduct -> {
+                R.id.itemAddSalesPerson -> {
                     val model: ItemViewModel by viewModels()
                     Log.d("aiuda", "( Al presionar el botón add )- Estos son los productos: ${marketStallPerson.products}")
                     model.setMarketStall(marketStallPerson)
@@ -125,6 +113,19 @@ class MenuActivitySalesPerson : AppCompatActivity() {
                     fragmentTransaction.replace(R.id.container, editMarketStallFragment())
                     fragmentTransaction.commit()
                     toolbar.title = "Editar puesto de venta"
+                    hideSoftKeyboard()
+                    true
+                }
+                R.id.itemAddSalesPerson -> {
+                    val model: ItemViewModel by viewModels()
+                    Log.d("aiuda", "( Al presionar el botón add )- Estos son los productos: ${marketStallPerson.products}")
+                    model.setMarketStall(marketStallPerson)
+                    viewBinding.drawer.closeDrawer(GravityCompat.START)
+                    fragmentManagerInit = getSupportFragmentManager()
+                    fragmentTransaction = fragmentManagerInit.beginTransaction()
+                    fragmentTransaction.replace(R.id.container, AddSalesPersonFragment())
+                    fragmentTransaction.commit()
+                    toolbar.title = "Productos"
                     hideSoftKeyboard()
                     true
                 }
